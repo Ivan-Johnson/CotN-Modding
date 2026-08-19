c5ec22bee8bbda61965ef7a10501377de42276a5
Ivan Johnson <git@IvanJohnson.net>
2026-08-19 19:25:59 -0400

    Setup dev env

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+	description = "IDK. Something to do with CotN mods & vibe coding.";
+
+	inputs = {
+		nixpkgs.url = "nixpkgs/nixos-26.05";
+	};
+
+	outputs =
+		{ self, nixpkgs }:
+		let
+			pkgs = import nixpkgs { system = "x86_64-linux"; };
+
+			shell = pkgs.mkShell {
+				buildInputs = with pkgs; [
+					pkgs.coreutils
+					pkgs.which
+					pkgs.bash
+					pkgs.git
+					pkgs.nix
+					pkgs.nixfmt
+				];
+				shellHook = "";
+			};
+		in
+		{
+			devShells.x86_64-linux.default = shell;
+
+			# TODO - actually build something useful here
+			packages.x86_64-linux.default = shell;
+		};
+}
