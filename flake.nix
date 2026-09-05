7cbdfef041b024613e548e1d52bd4cc5ef4a7b9b
Ivan Johnson <git@IvanJohnson.net>
2026-09-05 13:04:57 -0400

    Run nix formatter

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-	description = "IDK. Something to do with CotN mods & vibe coding.";
+        description = "IDK. Something to do with CotN mods & vibe coding.";
 
         inputs = {
                 nixpkgs.url = "nixpkgs/nixos-26.05";
@@ -10,44 +10,48 @@
                 };
         };
 
-	outputs =
-		{ self, nixpkgs, itj_dev_tools }:
-		let
-			pkgs = import nixpkgs { system = "x86_64-linux"; };
-			helloWorldModZip = pkgs.runCommand "hello-world-mod-zip" { nativeBuildInputs = [ pkgs.zip ]; } ''
-				mkdir -p "$out"
-				cd ${./HelloWorldMod}
-				zip -qr "$out/HelloWorldMod.zip" .
-			'';
+        outputs =
+                {
+                        self,
+                        nixpkgs,
+                        itj_dev_tools,
+                }:
+                let
+                        pkgs = import nixpkgs { system = "x86_64-linux"; };
+                        helloWorldModZip = pkgs.runCommand "hello-world-mod-zip" { nativeBuildInputs = [ pkgs.zip ]; } ''
+                                mkdir -p "$out"
+                                cd ${./HelloWorldMod}
+                                zip -qr "$out/HelloWorldMod.zip" .
+                        '';
 
-			shell = pkgs.mkShell {
-				buildInputs = with pkgs; [
+                        shell = pkgs.mkShell {
+                                buildInputs = with pkgs; [
                                         itj_dev_tools.packages.${pkgs.stdenv.hostPlatform.system}.default
 
-					# For packaging mods
-					pkgs.zip
-					pkgs.unzip
-				];
-				shellHook = ''
-					alias 'build-install="nix build && nix run .#install-hello-world-mod"'
-				'';
-			};
-		in
-		{
-			devShells.x86_64-linux.default = shell;
+                                        # For packaging mods
+                                        pkgs.zip
+                                        pkgs.unzip
+                                ];
+                                shellHook = ''
+                                        alias 'build-install="nix build && nix run .#install-hello-world-mod"'
+                                '';
+                        };
+                in
+                {
+                        devShells.x86_64-linux.default = shell;
 
-			packages.x86_64-linux.default = helloWorldModZip;
+                        packages.x86_64-linux.default = helloWorldModZip;
 
-			apps.x86_64-linux.install-hello-world-mod = {
-				type = "app";
-				program = "${pkgs.writeShellScriptBin "install-hello-world-mod" ''
-					set -eu
+                        apps.x86_64-linux.install-hello-world-mod = {
+                                type = "app";
+                                program = "${pkgs.writeShellScriptBin "install-hello-world-mod" ''
+                                        set -eu
 
-					target_dir="$HOME/.local/share/NecroDancer/downloadedMods"
+                                        target_dir="$HOME/.local/share/NecroDancer/downloadedMods"
 
-					mkdir -p "$target_dir"
-					cp -f ${helloWorldModZip}/HelloWorldMod.zip "$target_dir/HelloWorldMod.zip"
-				''}/bin/install-hello-world-mod";
-			};
-		};
+                                        mkdir -p "$target_dir"
+                                        cp -f ${helloWorldModZip}/HelloWorldMod.zip "$target_dir/HelloWorldMod.zip"
+                                ''}/bin/install-hello-world-mod";
+                        };
+                };
 }
