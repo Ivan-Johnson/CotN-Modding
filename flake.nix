a7a892cc26e364076ebf958f384c27386ce040ae
Ivan Johnson <git@IvanJohnson.net>
2026-09-06 18:57:46 -0400

    Add appropriate paths to LLM sandboxes

--- flake.nix
+++ flake.nix
@@ -130,7 +130,12 @@
                                 # conversion is broken, which is exactly when it is needed. It is
                                 # built on demand instead.
                                 shellHook = ''
+                                        # This path is platform specific. e.g. if you're running the native steam runtime vs proton
+                                        # todo: commonize?
+                                        COTN_LOCAL_MODS_DIR="$HOME/.local/share/Steam/steamapps/compatdata/247080/pfx/drive_c/users/steamuser/AppData/Local/NecroDancer/mods/"
+
                                         export ITJ_GIT_PREPUSH_ENABLE_NIX_CHECKS=
+                                        export EXTRA_RO_BINDS="''${EXTRA_RW_BINDS:+''$EXTRA_RW_BINDS:}$COTN_LOCAL_MODS_DIR:/home/i/.local/share/Steam/steamapps/common/Crypt of the NecroDancer/NecroDancer64/NecroDancer.log"
 
                                         # Rebuild the documentation, then read a page from it.
                                         man-crypt() {
@@ -138,10 +143,6 @@
                                                 MANPATH="$out/share/man" man "$@"
                                         }
                                         export -f man-crypt
-
-                                        # This path is platform specific. e.g. if you're running the native steam runtime vs proton
-                                        # todo: commonize?
-                                        COTN_LOCAL_MODS_DIR="$HOME/.local/share/Steam/steamapps/compatdata/247080/pfx/drive_c/users/steamuser/AppData/Local/NecroDancer/mods/"
 
                                         # At present this doesn't actually "build" anything; I just want to avoid confusion with the regular `install` command
                                         alias 'build-install=${pkgs.rsync}/bin/rsync -rlt --delete "$ITJ_FLAKE_ROOT/Mods/HelloWorldMod" "$COTN_LOCAL_MODS_DIR"'
